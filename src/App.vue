005432809506576d0c98068fb560ea6e493ffb6d
Dan Bz <danbzurovski@gmail.com>
2020-05-18 14:09:11 -0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="bg-gray-200">
+    <CheckoutPage></CheckoutPage>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CheckoutPage from "./pages/CheckoutPage.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    CheckoutPage
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
