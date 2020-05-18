1789c3f34a255d301766ecb848af34497dc61b37
Dan Bz <danbzurovski@gmail.com>
2020-05-18 14:10:22 -0300

    Create Sumary and Item Component

---
+++ src/components/Item.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="grid grid-cols-8 overflow-hidden col-gap-6 mb-5">
+    <img class="block col-span-3 w-full h-20 rounded bg-gray-600" src alt />
+    <div class="col-span-5">
+      <div class="flex">
+        <h6 class="text-sm font-semibold text-gray-800 flex-grow">Item title</h6>
+        <button>
+          <svg class="w-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path
+              d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"
+            />
+          </svg>
+        </button>
+      </div>
+      <p
+        class="text-xs truncate text-gray-600"
+      >Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe vero possimus repellat nam nisi odit aspernatur fugiat, neque praesentium id, commodi voluptate a error qui, debitis at eveniet ratione nemo!n</p>
+      <span class="text-sm text-gray-800">USD 10</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Item"
+};
+</script>
---
+++ src/components/Sumary.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="col-span-1 lg:col-span-4">
+    <h4 class="text-3xl text-gray-700 mb-5">Order Sumary</h4>
+    <div class="p-10 rounded-md shadow-md bg-white">
+      <item :key="i" v-for="(item, i) in items"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Item from "./Item";
+export default {
+  name: "Sumary",
+  components: {
+    Item
+  },
+  data() {
+    return {
+      items: [1, 1, 1]
+    };
+  }
+};
+</script>
